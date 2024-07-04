f6d15e40ba873ed1c48e828cfde57cafccf592f8
xingyifan <xingyifan10@qq.com>
2024-07-04 15:53:09 +0800

    feat:index页面中直接加loading

--- src/utils/loading/index.tsx
+++ src/utils/loading/index.tsx
@@ -1,20 +1,37 @@
-import ReactDom from 'react-dom/client'
-import Loading from './loading'
+// import ReactDom from 'react-dom/client'
+// import Loading from './loading'
+import './loading.less'
 
 let count = 0
 
 export const showLoading = () => {
   if (count === 0) {
-    const loading = document.createElement('div')
-    loading.setAttribute('id', 'loading')
-    document.body.appendChild(loading)
-    ReactDom.createRoot(loading).render(<Loading />)
+    const loading = document.getElementById('loading')
+    loading?.style.setProperty('display', 'flex')
   }
   count++
 }
 
 export const hideLoading = () => {
-  if (count < 0) return
   count--
-  if (count === 0) document.body.removeChild(document.getElementById('loading') as HTMLDivElement)
+  if (count === 0) {
+    const loading = document.getElementById('loading')
+    loading?.style.setProperty('display', 'none')
+  }
 }
+
+// export const showLoading = () => {
+//   if (count === 0) {
+//     const loading = document.createElement('div')
+//     loading.setAttribute('id', 'loading')
+//     document.body.appendChild(loading)
+//     ReactDom.createRoot(loading).render(<Loading />)
+//   }
+//   count++
+// }
+
+// export const hideLoading = () => {
+//   if (count < 0) return
+//   count--
+//   if (count === 0) document.body.removeChild(document.getElementById('loading') as HTMLDivElement)
+// }
