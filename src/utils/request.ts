57892cb79f3bd0776f168b5bcf7932452e63dc53
xingyifan <xingyifan10@qq.com>
2024-07-04 13:49:27 +0800

    feat:配置拦截器

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,4 +1,5 @@
-import axios from 'axios'
+import { message } from 'antd'
+import axios, { AxiosError } from 'axios'
 
 // 创建实例
 const instance = axios.create({
@@ -6,6 +7,36 @@ const instance = axios.create({
   timeout: 8000,
   timeoutErrorMessage: '请求超时，请稍后再试',
   withCredentials: true
+})
+
+// 请求拦截器
+instance.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem('token')
+    if (token) {
+      config.headers.Authorization = 'Token::' + token
+    }
+    return {
+      ...config
+    }
+  },
+  (error: AxiosError) => {
+    return Promise.reject(error)
+  }
+)
+
+// 响应拦截器
+instance.interceptors.response.use(response => {
+  const data = response.data
+  if (data.code === 500001) {
+    message.error(data.msg)
+    localStorage.removeItem('token')
+    location.href = '/login'
+  } else if (data.code !== 0) {
+    message.error(data.msg)
+    return Promise.reject(data)
+  }
+  return data.data
 })
 
 const request = {
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,17 +1,12 @@
 import { useEffect } from 'react'
 import request from '@/utils/request'
-import axios from 'axios'
 
 const Login = () => {
   useEffect(() => {
-    request
-      .get('/users', {
-        id: 12345,
-        pass: '1123'
-      })
-      .catch(error => {
-        console.log('error', error)
-      })
+    request.post('/users/login', {
+      id: 12345,
+      pass: '1123'
+    })
   }, [])
   return <div>Login111</div>
 }
